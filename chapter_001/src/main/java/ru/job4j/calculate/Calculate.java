4c6ea3f7f88ccc40f808614d1190733e669346d8
IzzyNety <izzy.nety@gmail.com>
2018-05-29 21:48:14 +0500

    1.3 JavaDoc.Code convention [4087]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,5 +1,13 @@
 package ru.job4j.Calculate;
 
+/**
+* Class Calculate для вывода сообщения на экран.
+* @author Astaafiev Ilya.
+* @since 29.05.2018.
+* @version 1
+* @param args - args
+*/
+
 public class Calculate {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Ilya Astafiev.
+* @version $id$.
+* @since 0.1
+*/
+package ru.job4j.calculate;
