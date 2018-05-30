2269f8f54ce2e19dc662b509d137dab21290709d
IzzyNety <izzy.nety@gmail.com>
2018-05-30 20:48:22 +0500

    1.3 JavaDoc. Code convention v2 [#4087]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -4,11 +4,13 @@ package ru.job4j.Calculate;
 * Конструктор, вывод строки на консоль.
 * @author Astaafiev Ilya.
 * @since 29.05.2018.
-* Main.
-* @param args - args
 */
 
 public class Calculate {
+/**
+* Main.
+* @param args - args
+*/
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
