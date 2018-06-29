ee55776fec1f4063948df1d2f303ec4ab2b6cbfe
IzzyNety <izzy.nety@gmail.com>
2018-06-29 18:21:53 +0500

    5.1 Подсчет суммы четных чисел в диапазоне [#192] v2

--- chapter_001/src/main/java/ru/job4j/loop/Counter.java
+++ chapter_001/src/main/java/ru/job4j/loop/Counter.java
@@ -3,7 +3,7 @@ package ru.job4j.loop;
 public class Counter {
     public int add(int start, int finish) {
         for (int index = start; index <= finish; index++) {
-            if (index % 2 <= 0) {
+            if (index % 2 == 0) {
                 start += index;
             }
         }
