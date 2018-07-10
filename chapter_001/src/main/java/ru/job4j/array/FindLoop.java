0015ae86f551fc07dd07dce1d3b5ae35bbbd7f54
IzzyNety <izzy.nety@gmail.com>
2018-07-10 19:15:29 +0500

    6.1 Классический поиск перебором. [#33489]

---
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -0,0 +1,22 @@
+package ru.job4j.array;
+
+/**
+ * @author Ilya Astafiev (izzy.nety@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class FindLoop {
+    /**
+     * Поиск элемента в массиве.
+    */
+    public int indexOf(int[] data, int el) {
+        int rst = -1;
+        for (int index = 0; index < data.length; index++) {
+            if (data[index] == el) {
+                rst = index;
+                break;
+            }
+        }
+        return rst;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
@@ -0,0 +1,45 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class FindLoopTest {
+    /**
+     * тест на нахождение элемента 5 в массиве
+     */
+    @Test
+    public void whenArrayHasLengh5Then0() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[] {5, 10, 3};
+        int value = 5;
+        int result = find.indexOf(input, value);
+        int expect = 0;
+        assertThat(result, is(expect));
+    }
+    /**
+     * тест на нахождения элемента 7 в массиве.
+     */
+    @Test
+    public void whenArrayHasLengh7then0() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[] {7, 10, 4};
+        int value = 7;
+        int result = find.indexOf(input, value);
+        int expect = 0;
+        assertThat(result, is(expect));
+    }
+    /**
+     * тест если элемент не найден
+     */
+    @Test
+    public void whenArrayHasNotLenght7ThenMinysOne() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[] {5, 10, 12};
+        int value = 7;
+        int result = find.indexOf(input, value);
+        int expect = -1;
+        assertThat(result, is(expect));
+    }
+}
