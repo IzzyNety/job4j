bddc78826f524ef795963cbed83184131e8f35ff
IzzyNety <izzy.nety@gmail.com>
2018-09-26 19:33:06 +0500

    7.1 Доп. задание №2 : Обьеденинить два массива в один и отсортировать 2

--- chapter_001/src/main/java/ru/job4j/array/ArrayPlusArray.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayPlusArray.java
@@ -1,20 +1,20 @@
 package ru.job4j.array;
 
 /**
- * Объединение двух массивов и последующая сортировка
+ * Объединение двух массивов в третий и последующая сортировка
  */
 
 public class ArrayPlusArray {
-    public int[] plusArray(int[] array1, int[] array2){
+    public int[] plusArray(int[] array1, int[] array2) {
         int[] array3 = new int[array1.length + array2.length];
-            for (int i = 0; i < array3.length; i++)
-                for (int j = 0; j < i; j++) {
-                    if (array3[j] > array3[j + 1]) {
-                        int tpm = array3[j];
-                        array3[j] = array3[j + 1];
-                        array3[j + 1] = tpm;
-                    }
+            for (int i = 0; i < array1.length; i++) {
+                array3[i] = array1[i];
+            }
+                for (int i = 0; i < array2.length; i++) {
+                    array3[i + array1.length] = array2[i];
                 }
-        return array3;
+                BubbleSort sort = new BubbleSort();
+                sort.sort(array3);
+                return array3;
     }
 }
--- chapter_001/src/test/java/ru/job4j/array/ArrayPlusArrayTest.java
+++ chapter_001/src/test/java/ru/job4j/array/ArrayPlusArrayTest.java
@@ -7,15 +7,15 @@ import static org.junit.Assert.assertThat;
 
 public class ArrayPlusArrayTest {
     /**
-     * тест на сложение двух массивов и последующей сортировки
+     * тест на сложение двух массивов в третий и последующей сортировки
      */
     @Test
-    public void whenOneArrayPlusTwoArrayThenSortArray(){
+    public void whenOneArrayPlusTwoArrayThenSortArray() {
         ArrayPlusArray arrayPlusArray = new ArrayPlusArray();
-        int[] input = new int[]{1, 5, 4};
-        int[] input2 = new int[]{2, 3, 6};
-        int[] result = arrayPlusArray.plusArray(input,input2);
-        int[] expect = new  int[]{1,2,3,4,5,6};
+        int[] input = new int[]{1, 2, 3};
+        int[] input2 = new int[]{4, 5, 6};
+        int[] result = arrayPlusArray.plusArray(input, input2);
+        int[] expect = new  int[]{1, 2, 3, 4, 5, 6};
         assertThat(result, is(expect));
     }
 }
