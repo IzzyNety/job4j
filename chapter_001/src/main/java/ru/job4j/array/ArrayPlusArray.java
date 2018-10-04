ae8c374737b9f6d9caf69033d6466ed8e839ba7a
IzzyNety <izzy.nety@gmail.com>
2018-10-04 18:51:07 +0500

    7.1 Доп. задание №2 : Обьеденинить два массива в один и отсортировать 3

--- chapter_001/src/main/java/ru/job4j/array/ArrayPlusArray.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayPlusArray.java
@@ -1,20 +1,40 @@
 package ru.job4j.array;
 
 /**
- * Объединение двух массивов в третий и последующая сортировка
+ * Объединение двух массивов в третий по возрастанию
  */
 
 public class ArrayPlusArray {
     public int[] plusArray(int[] array1, int[] array2) {
         int[] array3 = new int[array1.length + array2.length];
-            for (int i = 0; i < array1.length; i++) {
+        int i = 0, j = 0;
+        for (int k = 0; k < array3.length; k++) {
+            if (i > array1.length - 1) {
+                int a = array2[j];
+                array3[k] = a;
+                j++;
+            } else if (j > array2.length - 1) {
+                int a = array1[i];
+                array3[k] = a;
+                i++;
+            } else if (array1[i] < array2[j]) {
+                int a = array1[i];
+                array3[k] = a;
+                i++;
+            } else {
+                int b = array2[j];
+                array3[k] = b;
+                j++;
+            }
+        }
+  /**          for (int i = 0; i < array1.length; i++) {
                 array3[i] = array1[i];
             }
-                for (int i = 0; i < array2.length; i++) {
+            for (int i = 0; i < array2.length; i++) {
                     array3[i + array1.length] = array2[i];
                 }
-                BubbleSort sort = new BubbleSort();
-                sort.sort(array3);
+
+*/
                 return array3;
     }
 }
--- chapter_001/src/test/java/ru/job4j/array/ArrayPlusArrayTest.java
+++ chapter_001/src/test/java/ru/job4j/array/ArrayPlusArrayTest.java
@@ -18,4 +18,49 @@ public class ArrayPlusArrayTest {
         int[] expect = new  int[]{1, 2, 3, 4, 5, 6};
         assertThat(result, is(expect));
     }
+    @Test
+    public void whenOneArrayHaveThreeAndPlusArrayHaveFive() {
+        ArrayPlusArray arrayPlusArray = new ArrayPlusArray();
+        int[] input = new int[]{1, 4, 6};
+        int[] input2 = new int[]{2, 3, 5, 7, 11};
+        int[] result = arrayPlusArray.plusArray(input, input2);
+        int[] expect = new int[]{1, 2, 3, 4, 5, 6, 7, 11};
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void whenOneArrayHaveFiveAndPlusArrayHavethree() {
+        ArrayPlusArray arrayPlusArray = new ArrayPlusArray();
+        int[] input = new int[]{2, 3, 5, 7, 11};
+        int[] input2 = new int[]{1, 4, 6};
+        int[] result = arrayPlusArray.plusArray(input, input2);
+        int[] expect = new int[]{1, 2, 3, 4, 5, 6, 7, 11};
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void whenOneArrayHaveNullAndPlusArrayHaveThree() {
+        ArrayPlusArray arrayPlusArray = new ArrayPlusArray();
+        int[] input = new int[]{};
+        int[] input2 = new int[]{1, 3, 5};
+        int[] result = arrayPlusArray.plusArray(input, input2);
+        int[] expect = new int[]{1, 3, 5};
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void whenArrayHaveThreeAndPlusArrayHaveNull() {
+        ArrayPlusArray arrayPlusArray = new ArrayPlusArray();
+        int[] input = new int[]{1, 3, 5};
+        int[] input2 = new int[]{};
+        int[] result = arrayPlusArray.plusArray(input, input2);
+        int[] expect = new int[]{1, 3, 5};
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void whenArrayHaveNullAndPlusArrayHaveNull() {
+        ArrayPlusArray arrayPlusArray = new ArrayPlusArray();
+        int[] input = new int[]{};
+        int[] input2 = new int[]{};
+        int[] result = arrayPlusArray.plusArray(input, input2);
+        int[] expect = new int[]{};
+        assertThat(result, is(expect));
+    }
 }
