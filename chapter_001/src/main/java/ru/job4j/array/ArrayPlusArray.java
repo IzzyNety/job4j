7c4816c2875558b7f36a778ad0cf155bc812d269
IzzyNety <izzy.nety@gmail.com>
2018-10-05 18:24:50 +0500

    7.1 Доп. задание №2 : рефакторинг кода

--- chapter_001/src/main/java/ru/job4j/array/ArrayPlusArray.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayPlusArray.java
@@ -5,36 +5,23 @@ package ru.job4j.array;
  */
 
 public class ArrayPlusArray {
-    public int[] plusArray(int[] array1, int[] array2) {
-        int[] array3 = new int[array1.length + array2.length];
+    public int[] plusArray(int[] first, int[] second) {
+        int[] result = new int[first.length + second.length];
         int i = 0, j = 0;
-        for (int k = 0; k < array3.length; k++) {
-            if (i > array1.length - 1) {
-                int a = array2[j];
-                array3[k] = a;
-                j++;
-            } else if (j > array2.length - 1) {
-                int a = array1[i];
-                array3[k] = a;
-                i++;
-            } else if (array1[i] < array2[j]) {
-                int a = array1[i];
-                array3[k] = a;
-                i++;
+        for (int k = 0; k < result.length; k++) {
+            if (i > first.length - 1) {
+                result[k] = second[j++];
+
+            } else if (j > second.length - 1) {
+                result[k] = first[i++];
+
+            } else if (first[i] < second[j]) {
+                result[k] = first[i++];
             } else {
-                int b = array2[j];
-                array3[k] = b;
-                j++;
+                result[k] = second[j++];
             }
         }
-  /**          for (int i = 0; i < array1.length; i++) {
-                array3[i] = array1[i];
-            }
-            for (int i = 0; i < array2.length; i++) {
-                    array3[i + array1.length] = array2[i];
-                }
 
-*/
-                return array3;
+                return result;
     }
 }
