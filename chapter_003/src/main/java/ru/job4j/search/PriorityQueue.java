495dca410ed1733fc15503bebb5b5d345f76661c
IzzyNety <izzy.nety@gmail.com>
2019-05-16 19:48:30 +0500

    2. Очередь с приоритетом на LinkedList [#41670]

---
+++ chapter_003/src/main/java/ru/job4j/search/PriorityQueue.java
@@ -0,0 +1,22 @@
+package ru.job4j.search;
+
+import java.util.LinkedList;
+
+public class PriorityQueue {
+    private LinkedList<Task> tasks = new LinkedList<>();
+
+    public void put(Task task) {
+        int i = tasks.size();
+        for (int index = 0; index < tasks.size(); index++) {
+            if(task.getPriority() < tasks.get(index).getPriority()) {
+                i = index;
+                break;
+            }
+        }
+        tasks.add(i, task);
+    }
+
+    public Task take() {
+        return this.tasks.poll();
+    }
+}
